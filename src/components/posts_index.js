d58d581306a5f46bd168171c934a74bff94efed1
Robbie Heygate <r.heygate.dev@gmail.com>
2017-07-04 17:29:16 +0100

    Begin form build

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
   
   componentDidMount() {
-    console.log('running');
     this.props.fetchPosts();
   }
   
+  renderPosts() {
+    return _.map(this.props.posts, post=> {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+  
   render() {
-    console.log(this.props.posts)
     return (
       
         <div>
-          Posts Index
+          <div className="text-xs-right">
+            <Link className="btn btn-primary" to="/posts/new">
+              Add a Post
+            </Link>
+          </div>
+          <h3>Posts</h3>
+          <ul className="list-group">
+            {this.renderPosts()}
+          </ul>
         </div>
     )
   }
